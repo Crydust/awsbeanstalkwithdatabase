9306d2ecfeba99735986c5365bae6d78db8343ce
Kristof Neirynck <k.neirynck@gmail.com>
2018-05-27 14:01:46 +0200

    Cleanup output from HelloServlet

--- src/main/java/be/crydust/spike/HelloServlet.java
+++ src/main/java/be/crydust/spike/HelloServlet.java
@@ -1,6 +1,8 @@
 package be.crydust.spike;
 
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 import static javax.servlet.http.HttpServletResponse.SC_METHOD_NOT_ALLOWED;
+import static org.apache.commons.text.StringEscapeUtils.escapeHtml4;
 
 import java.io.IOException;
 import java.util.Date;
@@ -14,12 +16,29 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "HelloServlet", urlPatterns = {"/HelloServlet"})
 public class HelloServlet extends HttpServlet {
+
+	private static void writeResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		final ServletOutputStream out = response.getOutputStream();
+		final String indexUrl = response.encodeURL(request.getContextPath() + "/index.jsp");
+		out.print("<!DOCTYPE html>\n" +
+				"<html>\n" +
+				"<head>\n" +
+				"<title>HelloServlet</title>\n" +
+				"<meta charset=\"UTF-8\"/>\n" +
+				"</head>\n" +
+				"<body>\n" +
+				"<h1>HelloServlet</h1>\n" +
+				"<p>Hello World! " + escapeHtml4(Long.toString(System.currentTimeMillis())) + "</p>\n" +
+				"<p><a href='" + escapeHtml4(indexUrl) + "'>Go back to the index</a></p>\n" +
+				"</body>\n" +
+				"</html>");
+	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 		response.setStatus(SC_METHOD_NOT_ALLOWED);
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		final ServletOutputStream out = response.getOutputStream();
-		out.print("Hello World! " + System.currentTimeMillis());
+		writeResponse(request, response);
 	}
 }
